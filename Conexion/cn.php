c3b33b09739c7fa3a7282c8bc7a8780a28f7ea2b
FranciscoMartinez-650 <paquito.martinez2502.pm@gmail.com>
2021-06-18 21:34:42 -0500

    Cambio numero 1

--- Conexion/cn.php
+++ Conexion/cn.php
@@ -1,7 +1,7 @@
 <?php
     //function conectar(){
     $conexion = new mysqli
-    ('localhost','root','','Cafe');
+    ('localhost','root','americayya99','Cafe');
     $acentos = $conexion ->query("set names 'utf8'");
     if($conexion ->connect_error){
         
