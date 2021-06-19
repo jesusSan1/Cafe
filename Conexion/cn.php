c9f627d26c2c7d383eec101d17817407318fe0f3
jesus_san1 <jjsanru3@gmail.com>
2021-06-19 12:15:49 -0500

    Agregado el archivo de conexion

---
+++ Conexion/cn.php
@@ -0,0 +1,11 @@
+<?php
+//function conectar(){
+$conexion = new mysqli
+    ('localhost', 'root', '', 'Cafe');
+$acentos = $conexion->query("set names 'utf8'");
+if ($conexion->connect_error) {
+
+    die('Error en la conexion: ' .
+        $conexion->connect_error);
+
+}
